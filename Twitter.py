23f38d07050421f10ce906b657c693b0ee6eee1f
Trell Hudson <thudson@axialhealthcare.com>
2017-09-21 09:33:24 -0500

    Twitter.py

---
+++ Twitter.py
@@ -0,0 +1,24 @@
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'stream_twitter',
+    'stream_django'
+)
+
+class Tweet(models.Model):
+    user = models.ForeignKey('auth.User')
+    text = models.CharField(max_length=160)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Follow(models.Model):
+    user = models.ForeignKey('auth.User', related_name='friends')
+    target = models.ForeignKey('auth.User', related_name='followers')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'target')

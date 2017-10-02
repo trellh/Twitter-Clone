f59c1a467bc2428f048852ea884d695a014d35b9
Trell Hudson <thudson@axialhealthcare.com>
2017-10-02 13:48:28 -0500

    Twitter.py

--- Twitter.py
+++ Twitter.py
@@ -9,6 +9,18 @@ INSTALLED_APPS = (
     'stream_django'
 )
 
+from django.db import models, connection
+from django.conf import settings as django_settings
+from datetime import datetime, timedelta
+from email.utils import parsedate
+from django.utils import timezone
+import os
+import socket
+from . import settings
+from django.core.exceptions import ObjectDoesNotExist
+from swapper import swappable_setting
+from . import fields
+
 class Tweet(models.Model):
     user = models.ForeignKey('auth.User')
     text = models.CharField(max_length=160)
@@ -22,3 +34,16 @@ class Follow(models.Model):
 
     class Meta:
         unique_together = ('user', 'target')
+
+from django.views.generic.edit import CreateView
+from stream_twitter.models import Follow
+from stream_twitter.models import Tweet
+
+
+class TweetView(CreateView):
+    model = Tweet
+    fields = ['text']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(Tweet, self).form_valid(form)
